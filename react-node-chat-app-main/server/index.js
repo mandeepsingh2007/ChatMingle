ed691c7b2b65d4e73f01ab6835c10ad36a995643
Mandeep Singh <singh.mandeeep2007@gmail.com>
2025-04-10 00:25:11 +0530

    Update index.js

--- react-node-chat-app-main/server/index.js
+++ react-node-chat-app-main/server/index.js
@@ -22,7 +22,7 @@ app.use(
     credentials: true,
   })ererer
 );helloooggggggg
-ffgfgfdggfggdgdfg
+ffgfgfdggfggdgdfgddfsdfsdfsfsdf
 app.use("/uploads/profiles", express.static("uploads/profiles"));
 app.use("/uploads/files", express.static("uploads/files"));
 
